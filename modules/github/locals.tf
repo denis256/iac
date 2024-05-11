99fad1e57df62d38535486da8d07d5b29a2c5688
Marco Sanavia <sanaistheway@hotmail.it>
2024-05-11 12:49:04 +0100

    Github Updates

--- modules/github/locals.tf
+++ modules/github/locals.tf
@@ -2,4 +2,7 @@ locals {
   dependabot_enabled = {
     for k, v in var.github_repositories : k => v if v.dependabot_enable != false
   }
+  public_repositories ={
+    for k, v in var.github_repositories : k => v if v.visibility == "public"
+  }
 }
